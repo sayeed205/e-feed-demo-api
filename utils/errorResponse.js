18bd76a2bdb81245c3ed2b5bfd29d59162302ed8
Hitarashi <sayeed205@gmail.com>
2023-04-25 19:59:25 +0530

    Some utils and middleware

---
+++ utils/errorResponse.js
@@ -0,0 +1,8 @@
+class ErrRes extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export default ErrRes;
---
+++ utils/makeSchema.js
@@ -0,0 +1,9 @@
+const makeSchema = (object, keys) =>
+    keys.reduce((obj, key) => {
+        if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+            obj[key] = object[key];
+        }
+        return obj;
+    }, {});
+
+export default makeSchema;
